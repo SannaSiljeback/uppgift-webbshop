3fa12eb41be6bc7643cada9d57ee74acac7b9d85
Sanna Siljebäck <sannasiljeback@gmail.com>
2024-04-17 22:32:57 +0200

    installed express

--- mongodbconnection/index.js
+++ mongodbconnection/index.js
@@ -1,24 +1,33 @@
 console.log("index.js");
 
 let mongodb = require("mongodb");
+let express = require("express");
 
-let url = "mongodb://localhost:27017";
-let client = new mongodb.MongoClient(url);
+let app = express();
 
-client.connect().then(() => {
-  console.log("connected");
+app.get("/", (req, res) => {
+  let url = "mongodb://localhost:27017";
+  let client = new mongodb.MongoClient(url);
 
-  let db = client.db("shop");
-  let collection = db.collection("products");
+  client
+    .connect()
+    .then(() => {
+      console.log("connected");
 
-  return collection.insertMany([{a:1}]).then(() => {
-    console.log("inserted");
+      let db = client.db("shop");
+      let collection = db.collection("products");
 
-    return collection.find({a:1}).toArray().then((results) => {
-        console.log("found", results);
+      return collection
+        .find({ a: 1 })
+        .toArray()
+        .then((results) => {
+          console.log("found", results);
+          res.json(results);
+        });
     })
-  });
+    .finally(() => {
+      client.close();
+    });
+});
 
-}).finally(() => {
-    client.close();
-})
+app.listen(3000);
