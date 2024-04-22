a1be4785bb13130ba6a90cdb1ef2f6c2dab53607
Sanna Siljebäck <sannasiljeback@gmail.com>
2024-04-22 15:07:36 +0200

    mongoose with costumers

--- server/server.js
+++ server/server.js
@@ -1,191 +1,189 @@
-let mongodb = require("mongodb");
-let express = require("express");
-let app = express();
-let url = "mongodb://localhost:27017";
-let client = new mongodb.MongoClient(url);
-
-
+// const mongodb = require("mongodb");
+const express = require("express");
+const app = express();
+const url = "mongodb://localhost:27017/shop";
+// const client = new mongodb.MongoClient(url);
+const mongoose = require("mongoose");
+const Customers = require("./models/customers");
 
 //PRODUKTER
 // hämtar produkter
-app.get("/", (request, response) => {
-    let db = client.db("shop");
-    let collection = db.collection("products");
-    client
-      .connect()
-      .then(() => {
-        console.log("Connected");
-  
-        return collection
-          .find({}) //om vi vill hitta allt från listan lämnar vi detta tomt
-          .toArray()
-          .then((results) => {
-            console.log("Found", results);
-            response.send(results);
-          });
-      })
-      .finally(() => {
-        client.close();
-      });
-  });
-  
-  // Lägger till produkter
-  app.post("/create-product", (request, response) => {
-    client
-      .connect()
-      .then(async () => {
-        console.log("Connected");
-        let db = client.db("shop");
-        let collection = db.collection("products");
-  
-        await collection.insertOne({
-            _id: "123456",
-            name: "test produkt 10",
-            image: null,
-            inStock: 50,
-            price: 100,
-            status: "active",
-            description: "..."
-        });
-  
-        return collection
-          .find({}) //om vi vill hitta allt från listan lämnar vi detta tomt
-          .toArray()
-          .then((results) => {
-            console.log("Found", results);
-            response.send(results);
-          });
-      })
-      .finally(() => {
-        client.close();
-      });
-  });
-  
-  // Uppdaterar existerande produkter
-  app.put("/update-product", (request, response) => {
-    client
-      .connect()
-      .then(async () => {
-        console.log("Connected");
-        let db = client.db("shop");
-        let collection = db.collection("products");
-  
-        const filter = { _id: "1234" };
-        const options = { upsert: true };
-        const updateDoc = {
-          $set: {
-            description: `A harvest of random numbers, such as: ${Math.random()}`,
-          },
-        };
-  
-        await collection.updateOne(filter, updateDoc, options);
-  
-        return collection
-          .find({}) //om vi vill hitta allt från listan lämnar vi detta tomt
-          .toArray()
-          .then((results) => {
-            console.log("Found", results);
-            response.send(results);
-          });
-      })
-      .finally(() => {
-        client.close();
-      });
-  });
+app.get("/", (req, res) => {
 
+});
 
-  //ORDRAR
+// Lägger till produkter
+app.post("/create-product", (req, res) => {
 
+});
 
+// Uppdaterar existerande produkter
+app.put("/update-product", (req, res) => {
 
+});
 
+//ORDRAR
+//hämtar ordrar
+app.get("/orders", (req, res) => {
 
-//ANVÄNDARE
+});
+
+// Lägger till ordrar
+app.post("/create-order", (req, res) => {
+
+});
+
+// Uppdaterar existerande order
+app.put("/update-order", (req, res) => {
+  
+});
+
+//CUSTOMERS
 // Hämtar användare
-app.get("/customers", (request, response) => {
-  let db = client.db("shop");
-  let collection = db.collection("customers");
-  client
-    .connect()
-    .then(() => {
-      console.log("Connected");
+app.get("/customers", async (req, res) => {
+  try {
+    await mongoose
+      .connect(url)
+      .then(console.log("connected"));
 
-      return collection
-        .find({}) //om vi vill hitta allt från listan lämnar vi detta tomt
-        .toArray()
-        .then((results) => {
-          console.log("Found", results);
-          response.send(results);
-        });
-    })
-    .finally(() => {
-      client.close();
+    Customers.find().then((result) => {
+      res.send(result);
+      mongoose.connection.close();
     });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 // Lägger till användare
-app.post("/create-user", (request, response) => {
-  client
-    .connect()
-    .then(async () => {
-      console.log("Connected");
-      let db = client.db("shop");
-      let collection = db.collection("customers");
+app.post("/create-customer", async (req, res) => {
+  try {
+    await mongoose
+      .connect(url)
+      .then(console.log("connected"));
 
-      await collection.insertOne({
-        email: "test@test.com",
-        firstName: "Test",
-        lastName: "Testsson",
-        adress: "Testgatan",
-        houseNumber: "1",
-        password: "p4$$w0rd",
-      });
-
-      return collection
-        .find({}) //om vi vill hitta allt från listan lämnar vi detta tomt
-        .toArray()
-        .then((results) => {
-          console.log("Found", results);
-          response.send(results);
-        });
-    })
-    .finally(() => {
-      client.close();
+    const customer = new Customers({
+      _id: "test@testsson.test",
+      firstName: "Test",
+      lastName: "Testsson",
+      address: "Testgatan 1",
+      password: "1234",
     });
+
+    customer.save().then((result) => {
+      res.send(result);
+      mongoose.connection.close();
+    });
+
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 // Uppdaterar existerande användare
-app.put("/update-user", (request, response) => {
-  client
-    .connect()
-    .then(async () => {
-      console.log("Connected");
-      let db = client.db("shop");
-      let collection = db.collection("customers");
+app.put("/update-customer", async (req, res) => {
+  try {
+    await mongoose.connect(url).then(console.log("connected"));
 
-      const filter = { email: "test@test.com" };
-      const options = { upsert: true };
-      const updateDoc = {
-        $set: {
-          plot: `A harvest of random numbers, such as: ${Math.random()}`,
-        },
-      };
+    Customers.findByIdAndUpdate("test@testsson.test", {
+      firstName: "Sanna",
+      lastName: "Silje",
+      address: "Siljegatan 2"
+    }).then((result) => {
 
-      await collection.updateOne(filter, updateDoc, options);
-
-      return collection
-        .find({}) //om vi vill hitta allt från listan lämnar vi detta tomt
-        .toArray()
-        .then((results) => {
-          console.log("Found", results);
-          response.send(results);
-        });
-    })
-    .finally(() => {
-      client.close();
+      res.send(result);
+      mongoose.connection.close();
     });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
+// //PRODUKTER
+// // hämtar produkter
+// app.get("/", (request, response) => {
+//     let db = client.db("shop");
+//     let collection = db.collection("products");
+//     client
+//       .connect()
+//       .then(() => {
+//         console.log("Connected");
 
+//         return collection
+//           .find({}) //om vi vill hitta allt från listan lämnar vi detta tomt
+//           .toArray()
+//           .then((results) => {
+//             console.log("Found", results);
+//             response.send(results);
+//           });
+//       })
+//       .finally(() => {
+//         client.close();
+//       });
+//   });
+
+//   // Lägger till produkter
+//   app.post("/create-product", (request, response) => {
+//     client
+//       .connect()
+//       .then(async () => {
+//         console.log("Connected");
+//         let db = client.db("shop");
+//         let collection = db.collection("products");
+
+//         await collection.insertOne({
+//             _id: "123456",
+//             name: "test produkt 10",
+//             image: null,
+//             inStock: 50,
+//             price: 100,
+//             status: "active",
+//             description: "..."
+//         });
+
+//         return collection
+//           .find({}) //om vi vill hitta allt från listan lämnar vi detta tomt
+//           .toArray()
+//           .then((results) => {
+//             console.log("Found", results);
+//             response.send(results);
+//           });
+//       })
+//       .finally(() => {
+//         client.close();
+//       });
+//   });
+
+//   // Uppdaterar existerande produkter
+//   app.put("/update-product", (request, response) => {
+//     client
+//       .connect()
+//       .then(async () => {
+//         console.log("Connected");
+//         let db = client.db("shop");
+//         let collection = db.collection("products");
+
+//         const filter = { _id: "1234" };
+//         const options = { upsert: true };
+//         const updateDoc = {
+//           $set: {
+//             description: `A harvest of random numbers, such as: ${Math.random()}`,
+//           },
+//         };
+
+//         await collection.updateOne(filter, updateDoc, options);
+
+//         return collection
+//           .find({}) //om vi vill hitta allt från listan lämnar vi detta tomt
+//           .toArray()
+//           .then((results) => {
+//             console.log("Found", results);
+//             response.send(results);
+//           });
+//       })
+//       .finally(() => {
+//         client.close();
+//       });
+//   });
 
 app.listen(3000, () => console.log("Server is up and running..."));
