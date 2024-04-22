c4905b9cae9f04a23e5e2e83ac0dfddc720f3a00
Sanna Siljebäck <sannasiljeback@gmail.com>
2024-04-22 12:29:34 +0200

    made product routes

--- server/server.js
+++ server/server.js
@@ -4,89 +4,188 @@ let app = express();
 let url = "mongodb://localhost:27017";
 let client = new mongodb.MongoClient(url);
 
-// Hämtar användare
+
+
+//PRODUKTER
+// hämtar produkter
 app.get("/", (request, response) => {
     let db = client.db("shop");
-    let collection = db.collection("customers");
+    let collection = db.collection("products");
     client
-        .connect()
-        .then(() => {
-            console.log("Connected");
-
-            return collection
-                .find({}) //om vi vill hitta allt från listan lämnar vi detta tomt
-                .toArray()
-                .then((results) => {
-                    console.log("Found", results);
-                    response.send(results);
-                });
-        })
-        .finally(() => {
-            client.close();
+      .connect()
+      .then(() => {
+        console.log("Connected");
+  
+        return collection
+          .find({}) //om vi vill hitta allt från listan lämnar vi detta tomt
+          .toArray()
+          .then((results) => {
+            console.log("Found", results);
+            response.send(results);
+          });
+      })
+      .finally(() => {
+        client.close();
+      });
+  });
+  
+  // Lägger till produkter
+  app.post("/create-product", (request, response) => {
+    client
+      .connect()
+      .then(async () => {
+        console.log("Connected");
+        let db = client.db("shop");
+        let collection = db.collection("products");
+  
+        await collection.insertOne({
+            _id: "123456",
+            name: "test produkt 10",
+            image: null,
+            inStock: 50,
+            price: 100,
+            status: "active",
+            description: "..."
         });
+  
+        return collection
+          .find({}) //om vi vill hitta allt från listan lämnar vi detta tomt
+          .toArray()
+          .then((results) => {
+            console.log("Found", results);
+            response.send(results);
+          });
+      })
+      .finally(() => {
+        client.close();
+      });
+  });
+  
+  // Uppdaterar existerande produkter
+  app.put("/update-product", (request, response) => {
+    client
+      .connect()
+      .then(async () => {
+        console.log("Connected");
+        let db = client.db("shop");
+        let collection = db.collection("products");
+  
+        const filter = { _id: "1234" };
+        const options = { upsert: true };
+        const updateDoc = {
+          $set: {
+            description: `A harvest of random numbers, such as: ${Math.random()}`,
+          },
+        };
+  
+        await collection.updateOne(filter, updateDoc, options);
+  
+        return collection
+          .find({}) //om vi vill hitta allt från listan lämnar vi detta tomt
+          .toArray()
+          .then((results) => {
+            console.log("Found", results);
+            response.send(results);
+          });
+      })
+      .finally(() => {
+        client.close();
+      });
+  });
+
+
+  //ORDRAR
+
+
+
+
+
+//ANVÄNDARE
+// Hämtar användare
+app.get("/customers", (request, response) => {
+  let db = client.db("shop");
+  let collection = db.collection("customers");
+  client
+    .connect()
+    .then(() => {
+      console.log("Connected");
+
+      return collection
+        .find({}) //om vi vill hitta allt från listan lämnar vi detta tomt
+        .toArray()
+        .then((results) => {
+          console.log("Found", results);
+          response.send(results);
+        });
+    })
+    .finally(() => {
+      client.close();
+    });
 });
 
 // Lägger till användare
 app.post("/create-user", (request, response) => {
-    client
-        .connect()
-        .then(async () => {
-            console.log("Connected");
-            let db = client.db("shop");
-            let collection = db.collection("customers");
+  client
+    .connect()
+    .then(async () => {
+      console.log("Connected");
+      let db = client.db("shop");
+      let collection = db.collection("customers");
 
-            await collection.insertOne({
-                email: "test@test.com",
-                firstName: "Test",
-                lastName: "Testsson",
-                adress: "Testgatan",
-                houseNumber: "1",
-                password: "p4$$w0rd",
-            });
+      await collection.insertOne({
+        email: "test@test.com",
+        firstName: "Test",
+        lastName: "Testsson",
+        adress: "Testgatan",
+        houseNumber: "1",
+        password: "p4$$w0rd",
+      });
 
-            return collection
-                .find({}) //om vi vill hitta allt från listan lämnar vi detta tomt
-                .toArray()
-                .then((results) => {
-                    console.log("Found", results);
-                    response.send(results);
-                });
-        })
-        .finally(() => {
-            client.close();
+      return collection
+        .find({}) //om vi vill hitta allt från listan lämnar vi detta tomt
+        .toArray()
+        .then((results) => {
+          console.log("Found", results);
+          response.send(results);
         });
+    })
+    .finally(() => {
+      client.close();
+    });
 });
 
 // Uppdaterar existerande användare
 app.put("/update-user", (request, response) => {
-    client
-        .connect()
-        .then(async () => {
-            console.log("Connected");
-            let db = client.db("shop");
-            let collection = db.collection("customers");
+  client
+    .connect()
+    .then(async () => {
+      console.log("Connected");
+      let db = client.db("shop");
+      let collection = db.collection("customers");
 
-            const filter = { email: "test@test.com" };
-            const options = { upsert: true };
-            const updateDoc = {
-                $set: {
-                    plot: `A harvest of random numbers, such as: ${Math.random()}`,
-                },
-            };
+      const filter = { email: "test@test.com" };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          plot: `A harvest of random numbers, such as: ${Math.random()}`,
+        },
+      };
 
-            await collection.updateOne(filter, updateDoc, options);
+      await collection.updateOne(filter, updateDoc, options);
 
-            return collection
-                .find({}) //om vi vill hitta allt från listan lämnar vi detta tomt
-                .toArray()
-                .then((results) => {
-                    console.log("Found", results);
-                    response.send(results);
-                });
-        })
-        .finally(() => {
-            client.close();
+      return collection
+        .find({}) //om vi vill hitta allt från listan lämnar vi detta tomt
+        .toArray()
+        .then((results) => {
+          console.log("Found", results);
+          response.send(results);
         });
+    })
+    .finally(() => {
+      client.close();
+    });
 });
+
+
 
 app.listen(3000, () => console.log("Server is up and running..."));
