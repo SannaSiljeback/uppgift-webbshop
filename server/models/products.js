c4de9e9af1adc6f54ce2e0a7828dba01d0399a2c
Sanna Siljebäck <sannasiljeback@gmail.com>
2024-04-22 15:44:57 +0200

    mongoose orders and products

---
+++ server/models/products.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const productSchema = new Schema({
+    _id: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    inStock: {
+        type: Number,
+        required: true
+    },
+    status: {
+        type: String,
+        required: true
+    },
+}, { timestamps: true });
+
+const Products = mongoose.model('products', productSchema);
+module.exports = Products;
--- server/server.js
+++ server/server.js
@@ -5,46 +5,125 @@ const url = "mongodb://localhost:27017/shop";
 // const client = new mongodb.MongoClient(url);
 const mongoose = require("mongoose");
 const Customers = require("./models/customers");
+const Products = require("./models/products");
+const Orders = require("./models/orders");
 
 //PRODUKTER
 // hämtar produkter
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
+  try {
+    await mongoose.connect(url).then(console.log("connected"));
 
+    Products.find().then((result) => {
+      res.send(result);
+      mongoose.connection.close();
+    });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 // Lägger till produkter
-app.post("/create-product", (req, res) => {
+app.post("/create-product", async (req, res) => {
+  try {
+    await mongoose.connect(url).then(console.log("connected"));
 
+    const product = new Products({
+      _id: "321",
+      name: "test produkt 50",
+      description: "...",
+      price: 300,
+      image: "imgurl",
+      inStock: 50,
+      status: "active",
+    });
+
+    product.save().then((result) => {
+      res.send(result);
+      mongoose.connection.close();
+    });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 // Uppdaterar existerande produkter
-app.put("/update-product", (req, res) => {
+app.put("/update-product", async (req, res) => {
+  try {
+    await mongoose.connect(url).then(console.log("connected"));
 
+    Products.findByIdAndUpdate("321", {
+      description: "en rolig sak",
+    }).then((result) => {
+      res.send(result);
+      mongoose.connection.close();
+    });
+  } catch (error) {
+    console.log(error);
+  }
 });
+
 
 //ORDRAR
 //hämtar ordrar
-app.get("/orders", (req, res) => {
+app.get("/orders", async (req, res) => {
+  try {
+    await mongoose.connect(url).then(console.log("connected"));
 
+    Orders.find().then((result) => {
+      res.send(result);
+      mongoose.connection.close();
+    });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 // Lägger till ordrar
-app.post("/create-order", (req, res) => {
+app.post("/create-order", async (req, res) => {
+  try {
+    await mongoose.connect(url).then(console.log("connected"));
 
+    const order = new Orders({
+      _id: "6789",
+      customer: "test@testsson.test",
+      orderDate: new Date(),
+      status: "unpaid",
+      totalPrice: 20,
+      paymentId: "unpaid",
+    });
+
+    order.save().then((result) => {
+      res.send(result);
+      mongoose.connection.close();
+    });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 // Uppdaterar existerande order
-app.put("/update-order", (req, res) => {
-  
+app.put("/update-order", async (req, res) => {
+  try {
+    await mongoose.connect(url).then(console.log("connected"));
+
+    Orders.findByIdAndUpdate("6789", {
+      status: "paid",
+    }).then((result) => {
+      res.send(result);
+      mongoose.connection.close();
+    });
+  } catch (error) {
+    console.log(error);
+  }
 });
+
 
 //CUSTOMERS
 // Hämtar användare
 app.get("/customers", async (req, res) => {
   try {
-    await mongoose
-      .connect(url)
-      .then(console.log("connected"));
+    await mongoose.connect(url).then(console.log("connected"));
 
     Customers.find().then((result) => {
       res.send(result);
@@ -58,9 +137,7 @@ app.get("/customers", async (req, res) => {
 // Lägger till användare
 app.post("/create-customer", async (req, res) => {
   try {
-    await mongoose
-      .connect(url)
-      .then(console.log("connected"));
+    await mongoose.connect(url).then(console.log("connected"));
 
     const customer = new Customers({
       _id: "test@testsson.test",
@@ -74,7 +151,6 @@ app.post("/create-customer", async (req, res) => {
       res.send(result);
       mongoose.connection.close();
     });
-
   } catch (error) {
     console.log(error);
   }
@@ -88,9 +164,8 @@ app.put("/update-customer", async (req, res) => {
     Customers.findByIdAndUpdate("test@testsson.test", {
       firstName: "Sanna",
       lastName: "Silje",
-      address: "Siljegatan 2"
+      address: "Siljegatan 2",
     }).then((result) => {
-
       res.send(result);
       mongoose.connection.close();
     });
